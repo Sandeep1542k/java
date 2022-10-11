b4a7009f453e7678038557bc361fb26623607341
Sandeep1542k <sandeepkumar1542k@gmail.com>
2022-10-11 13:54:09 -0700

    java

---
+++ app.js
@@ -0,0 +1,19 @@
+var url;
+console.log(url);
+
+
+
+url=    "  www.iskillers.com "
+console.log(url);
+url=  " www.iskillers.com.pk "
+console.log(url)
+
+var counter=10
+console.log(counter)
+counter=10+"iskillers"
+console.log(counter)
+var a=10
+var b=20
+var c=30
+var add=a+b+c
+console.log(a+b+c)
